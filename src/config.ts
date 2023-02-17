53c370a05d982991c0032211264e8d6a1796e9af
Dragan Pecanac <dragan.pecanac@protonmail.ch>
2023-02-17 04:06:51 -0800

    initial commit

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import type { SocialObjects } from "./types";
 
 export const SITE = {
-  website: "https://astro-paper.pages.dev/",
-  author: "Sat Naing",
+  website: "https://wwwdedryu.com/",
+  author: "Dragan Pecanac",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  title: "Dragan Pecanac",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
@@ -20,39 +20,39 @@ export const LOGO_IMAGE = {
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: ` ${SITE.title} on Github`,
+    href: "https://github.com/dragan-pecanac",
+    linkTitle: `Find ${SITE.title} on Github`,
     active: true,
   },
   {
     name: "Facebook",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on Facebook`,
-    active: true,
+    active: false,
   },
   {
     name: "Instagram",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Instagram`,
+    href: "https://instagram.com/dragan.pecanac",
+    linkTitle: `Follow ${SITE.title} on Instagram`,
     active: true,
   },
   {
     name: "LinkedIn",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on LinkedIn`,
+    href: "https://linkedin.com/dragan-pecanac",
+    linkTitle: `Connect with ${SITE.title} on LinkedIn`,
     active: true,
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:dragan.pecanac@protonmail.ch",
     linkTitle: `Send an email to ${SITE.title}`,
-    active: false,
+    active: true,
   },
   {
     name: "Twitter",
     href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Twitter`,
-    active: false,
+    linkTitle: `Follow ${SITE.title} on Twitter`,
+    active: true,
   },
   {
     name: "Twitch",
---
+++ src/content/blog/codeeasy-entry-level-solutions.md
@@ -0,0 +1,16 @@
+---
+author: Dragan Pecanac
+pubDatetime: 2023-01-05T15:20:35Z
+title: CodeEasy Entry-Level Solutions
+postSlug: ""
+featured: true
+draft: false
+tags:
+  - c#
+  - programming
+ogImage: ""
+description:
+  How you can enable/disable light & dark mode; and customize color schemes
+  of AstroPaper theme.
+---
+<h1>Hello</h1>
